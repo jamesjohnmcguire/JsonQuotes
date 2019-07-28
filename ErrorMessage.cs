922f267d76cae5e46a115549fd38033c92dbf3ed
JamesMc <jamesjohnmcguire@gmail.com>
2019-07-28 20:28:01 +0900

    json quotes example.

---
+++ ErrorMessage.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonQuotes
+{
+	public class ErrorMessage
+	{
+
+		[JsonProperty("error")]
+		public string Error { get; set; }
+
+		[JsonProperty("error_description")]
+		public new string Description { get; set; }
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JsonQuotes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			// https://jsonlint.com/ claims this is valid json string
+			string tester = "{ \"error\":\"InternalServerError\",\"error_description\":\"<!doctype html>\r\n<html lang=\"en\">\r\n\"}";
+
+			ErrorMessage message = JsonConvert.DeserializeObject<ErrorMessage>(tester);
+		}
+	}
+}
