651199dda23c248d9f564552ff67c2f6d4e1c8c6
JamesMc <jamesjohnmcguire@gmail.com>
2019-10-06 00:32:43 +0900

    Updated test string to match original report.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace JsonQuotes
 			// https://jsonlint.com/ claims this is valid json string
 			string tester = "{ \"error\":\"InternalServerError\",\"error_description\":\"<!doctype html>\r\n<html lang=\"en\">\r\n\"}";
 
+			tester = "{ \"error\":\"InternalServerError\",\"error_description\":\"<html lang=\\\"en\\\">\"}";
 			ErrorMessage message = JsonConvert.DeserializeObject<ErrorMessage>(tester);
 		}
 	}
